4653fdbc73472056952d4aa0f7e8c9f531c67e46
xiaotian73 <68240536@qq.com>
2020-09-14 18:35:40 +0800

    1.初步认知go语言数据类型

---
+++ demo.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	//go语言学习 旅程开启。你好世界！
+	fmt.Println("hello world!")
+	/*
+		go语言对行代码注释
+		go数据类型简单认知
+		go变量赋值
+	*/
+	var lans string = "你好,我是一个字符串"
+	fmt.Println(lans)
+	var age int = 10
+	fmt.Println(age)
+	//布尔型变量默认值为否
+	var is_ok bool
+	fmt.Println(is_ok)
+}
